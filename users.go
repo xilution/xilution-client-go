e990dba084fa0bf9fbb5d4cac39e5dddcdc7c8e1
Todd Brunia <tbrunia@gmail.com>
2021-05-08 17:59:04 -0500

    fix: fixed base urls for clients and users

--- users.go
+++ users.go
@@ -10,7 +10,7 @@ import (
 func (xc *XilutionClient) CreateUser(organizationId *string, user *User) (*string, error) {
 	rb, _ := json.Marshal(user)
 
-	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/organizations/%s/users", ElephantBaseUrl, *organizationId), strings.NewReader(string(rb)))
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/organizations/%s/users", RhinoBaseUrl, *organizationId), strings.NewReader(string(rb)))
 
 	location, err := xc.doCreateRequest(req)
 	if err != nil {
@@ -21,7 +21,7 @@ func (xc *XilutionClient) CreateUser(organizationId *string, user *User) (*strin
 }
 
 func (xc *XilutionClient) GetUser(organizationId *string, userId *string) (*User, error) {
-	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/organizations/%s/users/%s", ElephantBaseUrl, *organizationId, *userId), nil)
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/organizations/%s/users/%s", RhinoBaseUrl, *organizationId, *userId), nil)
 
 	body, err := xc.doGetRequest(req)
 	if err != nil {
@@ -35,7 +35,7 @@ func (xc *XilutionClient) GetUser(organizationId *string, userId *string) (*User
 }
 
 func (xc *XilutionClient) GetUsers(organizationId *string, pageSize, pageNumber *int) (*FetchUsersResponse, error) {
-	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/organizations/%s/users?pageSize=%d&pageNumber=%d", ElephantBaseUrl, *organizationId, *pageSize, *pageNumber), nil)
+	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/organizations/%s/users?pageSize=%d&pageNumber=%d", RhinoBaseUrl, *organizationId, *pageSize, *pageNumber), nil)
 
 	body, err := xc.doGetRequest(req)
 	if err != nil {
@@ -51,7 +51,7 @@ func (xc *XilutionClient) GetUsers(organizationId *string, pageSize, pageNumber 
 func (xc *XilutionClient) UpdateUser(organizationId *string, user *User) error {
 	rb, _ := json.Marshal(user)
 
-	req, _ := http.NewRequest("PUT", fmt.Sprintf("%s/organizations/%s/users/%s", ElephantBaseUrl, *organizationId, user.ID), strings.NewReader(string(rb)))
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("%s/organizations/%s/users/%s", RhinoBaseUrl, *organizationId, user.ID), strings.NewReader(string(rb)))
 
 	err := xc.doNoContentRequest(req)
 	if err != nil {
@@ -62,7 +62,7 @@ func (xc *XilutionClient) UpdateUser(organizationId *string, user *User) error {
 }
 
 func (xc *XilutionClient) DeleteUser(organizationId *string, userId *string) error {
-	req, _ := http.NewRequest("DELETE", fmt.Sprintf("%s/organizations/%s/users/%s", ElephantBaseUrl, *organizationId, *userId), strings.NewReader(string("")))
+	req, _ := http.NewRequest("DELETE", fmt.Sprintf("%s/organizations/%s/users/%s", RhinoBaseUrl, *organizationId, *userId), strings.NewReader(string("")))
 
 	err := xc.doNoContentRequest(req)
 	if err != nil {
